aa53bbd7bbcc9aadeacc4166f45f9f88de446373
vedanta007 <vedantasingh007@gmail.com>
2025-01-07 22:33:05 +0530

    Add BreadcrumbSeparator to BreadcrumbHeader for improved navigation clarity

--- components/breadcrumb-header.tsx
+++ components/breadcrumb-header.tsx
@@ -2,7 +2,7 @@
 
 import { usePathname } from 'next/navigation'
 import React from 'react'
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from './ui/breadcrumb'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from './ui/breadcrumb'
 import { MobileSidebar } from './sidebar'
 
 function BreadCrumbHeader() {
@@ -20,6 +20,7 @@ function BreadCrumbHeader() {
                                     {path === '' ? 'home' : path}
                                 </BreadcrumbLink>
                             </BreadcrumbItem>
+                            {index !== paths.length - 1 && <BreadcrumbSeparator />}
                         </React.Fragment>
                     ))}
                 </BreadcrumbList>
