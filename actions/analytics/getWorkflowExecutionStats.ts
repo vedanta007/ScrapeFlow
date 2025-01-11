db84dfd887afa734dd1c99055965221e24b87b49
vedanta007 <vedantasingh007@gmail.com>
2025-01-11 14:48:33 +0530

    Add execution status chart component and update workflow execution stats retrieval

--- actions/analytics/getWorkflowExecutionStats.ts
+++ actions/analytics/getWorkflowExecutionStats.ts
@@ -35,24 +35,29 @@ export async function GetWorkflowExecutionStats(period: Period) {
         start: dateRange.startDate,
         end: dateRange.endDate
     })
-        .map((date) => format(date, dateFormat))
-        .reduce((acc, date) => {
-            acc[date] = {
-                success: 0,
-                failed: 0,
-            }
-            return acc
-        }, {} as any)
+    .map((date) => format(date, dateFormat))
+    .reduce((acc, date) => {
+        acc[date] = {
+            success: 0,
+            failed: 0,
+        }
+        return acc
+    }, {} as any)
 
-        executions.forEach(execution => {
-            const date = format(execution.startedAt!, dateFormat)
-            if (execution.status === WorkflowExecutionStatus.COMPLETED) {
-                stats[date].success++
-            }
-            else if (execution.status === WorkflowExecutionStatus.FAILED) {
-                stats[date].failed++
-            }
-        })
+    executions.forEach(execution => {
+        const date = format(execution.startedAt!, dateFormat)
+        if (execution.status === WorkflowExecutionStatus.COMPLETED) {
+            stats[date].success++
+        }
+        else if (execution.status === WorkflowExecutionStatus.FAILED) {
+            stats[date].failed++
+        }
+    })
 
-    return stats
+    const result = Object.entries(stats).map(([date, infos]) => ({
+        date,
+        ...infos,
+    }))
+
+    return result
 }
--- app/(dashboard)/(home)/_components/executionStatusChart.tsx
+++ app/(dashboard)/(home)/_components/executionStatusChart.tsx
@@ -1,13 +1,84 @@
 'use client'
 
 import { GetWorkflowExecutionStats } from '@/actions/analytics/getWorkflowExecutionStats'
+import { Card, CardHeader, CardContent, CardTitle, CardDescription } from '@/components/ui/card'
+import { ChartContainer, ChartConfig, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart'
+import { Layers2 } from 'lucide-react'
 import React from 'react'
+import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts'
 
 type ChartData = Awaited<ReturnType<typeof GetWorkflowExecutionStats>>
 
+const chartConfig: ChartConfig = {
+    success: {
+        label: 'Success',
+        color: 'hsl(var(--chart-2))',
+    },
+    failed: {
+        label: 'Failed',
+        color: 'hsl(var(--chart-1))',
+    },
+}
+
 function ExecutionStatusChart({ data }: { data: ChartData }) {
     return (
-        <div>ExecutionStatusChart</div>
+        <Card>
+            <CardHeader>
+                <CardTitle className='text-2xl font-bold flex items-center gap-2'>
+                    <Layers2 className='w-6 h-6 text-primary' />
+                    Workflow execution status
+                </CardTitle>
+                <CardDescription>
+                    Daily number of successful and failed workflow executions
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <ChartContainer config={chartConfig} className='max-h-[200px] w-full'>
+                    <AreaChart
+                        data={data}
+                        height={200}
+                        accessibilityLayer
+                        margin={{ top: 20 }}
+                    >
+                        <CartesianGrid vertical={false} />
+                        <XAxis
+                            dataKey={'date'}
+                            tickLine={false}
+                            axisLine={false}
+                            tickMargin={8}
+                            minTickGap={32}
+                            tickFormatter={(value) => {
+                                const date = new Date(value)
+                                return date.toLocaleDateString('en-US', {
+                                    month: 'short',
+                                    day: 'numeric',
+                                })
+                            }}
+                        />
+                        <ChartLegend content={<ChartLegendContent />} />
+                        <ChartTooltip content={<ChartTooltipContent className='w-[250px]' />} />
+                        <Area
+                            min={0}
+                            type={'bump'}
+                            fillOpacity={0.6}
+                            fill='var(--color-success)'
+                            stroke='var(--color-success)'
+                            dataKey={'success'}
+                            stackId={'a'}
+                        />
+                        <Area
+                            min={0}
+                            type={'bump'}
+                            fillOpacity={0.6}
+                            fill='var(--color-failed)'
+                            stroke='var(--color-failed)'
+                            dataKey={'failed'}
+                            stackId={'a'}
+                        />
+                    </AreaChart>
+                </ChartContainer>
+            </CardContent>
+        </Card>
     )
 }
 
